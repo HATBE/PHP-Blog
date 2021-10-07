92f42ffb0e2d120fcd46def0c45abd684d317a75
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2021-10-07 19:50:50 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'user');
+    define('DB_PASSWORD', 'pw');
+    define('DB_NAME', 'blog');
+
+    define('ROOT_PATH', '/');
+
+    define('SITE_NAME', 'COOL BLOG');
