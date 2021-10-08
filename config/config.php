1101eca9770d5e9b6a322e3ec6d3c7b6697e6133
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2021-10-08 07:59:26 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'htbUSR');
-    define('DB_PASSWORD', 'S$64UvpsRVT&PXxq4S6qUYw@psM^x%%RcwCw5fcqTW9ykrvxq');
+    define('DB_USER', 'user');
+    define('DB_PASSWORD', 'password');
     define('DB_NAME', 'blog');
 
     define('ROOT_PATH', '/');
